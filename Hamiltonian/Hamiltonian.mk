4595d652d1866645520156f90d1331966b6acd4b
waltergu <waltergu1989@gmail.com>
2015-11-10 22:12:41 +0800

    Unnecessary folder removed.

--- Hamiltonian/Hamiltonian.mk
+++
@@ -1,7 +0,0 @@
-LSPATH := ./Hamiltonian
-
-LLIBS := VCA_Fortran
-LIBS += $(LLIBS)
-$(LLIBS):
-	$(FCC) -c $(LSPATH)/$@.f90 -m $@
-	mv *.so $(LSPATH)/
--- Test/Bond.py
+++
@@ -1,7 +0,0 @@
-from BasicClass.BondPy import *
-def test_bond():
-    a=Bond(0,Point(0,rcoord=[0.0,0.0],icoord=[0.0,0.0]),Point(1,rcoord=[0.0,1.0],icoord=[0.0,0.0]))
-    print a
-    print a.rcoord
-    print a.icoord
-    print a.is_intra_cell()
