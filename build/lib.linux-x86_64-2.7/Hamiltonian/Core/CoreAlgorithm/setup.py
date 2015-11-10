9ebd61bf7e821213e432d6176ff2bb47ae3c210e
waltergu <waltergu1989@gmail.com>
2015-11-10 22:10:48 +0800

    Unnecessary folder removed.

--- build/lib.linux-x86_64-2.7/Hamiltonian/Core/CoreAlgorithm/setup.py
+++
@@ -1,9 +0,0 @@
-def configuration(parent_package='',top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    config=Configuration('CoreAlgorithm',parent_package,top_path)
-    config.add_extension('VCA_Fortran',['VCA_Fortran.f90'])
-    return config
-
-if __name__ == '__main__':
-    from numpy.distutils.core import setup
-    setup(configuration=configuration)
