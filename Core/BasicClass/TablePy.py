bc002b0c0590eb26ed90b7b0985a766c41052bdc
waltergu <waltergu1989@gmail.com>
2015-12-03 02:49:19 +0800

    Tree structure of Table removed.

--- Core/BasicClass/TablePy.py
+++ Core/BasicClass/TablePy.py
@@ -5,7 +5,7 @@ class Table(dict):
     '''
     This class provides the methods to get an index from its sequence number or vice versa.
     '''
-    def __init__(self,indices=[],dict={},f=None):
+    def __init__(self,indices=[],dicts=[],f=None):
         '''
         Constructor.
         Parameters:
@@ -18,27 +18,34 @@ class Table(dict):
                 If it is None, the order of the index in indices will be used as its sequence number.
         '''
         for i,v in enumerate(indices):
-            if f==None:
+            if f is None:
                 self[v]=i
             else:
                 self[v]=f(v)
-        self.update(dict)
+        for dict in dicts:
+            self.update(dict)
            
-def union(**kwargs):
+def union(tables,key=None):
     '''
     This function returns the union of index-sequence tables.
     '''
     result=Table()
-    sum=0
-    for k,v in kwargs.iteritems():
-        if isinstance(v,Table):
-            result[k]=v
-            count=0
-            for k1,v1 in v.iteritems():
-                if not isinstance(v1,Table): 
-                    result[k1]=v1+sum
+    if key is None:
+        sum=0
+        for table in tables:
+            if isinstance(table,Table):
+                count=0
+                for k,v in table.iteritems():
+                    result[k]=v+sum
                     count+=1
-            sum+=count
+                sum+=count
+    else:
+        for table in tables:
+            result.update(table)
+        buff={}
+        for i,k in enumerate(sorted([k for k in result.keys()],key=key)):
+            buff[k]=i
+        result.update(buff)
     return result
 
 def subset(table,mask):
@@ -46,24 +53,20 @@ def subset(table,mask):
     This function returns a certain subset of an index-sequence table according to the mask function.
     '''
     result=Table()
-    result['_superset']=table
     for k,v in table.iteritems():
-        if not isinstance(v,Table) and mask(k):
+        if mask(k):
             result[k]=v
     buff={}
-    for i,k in enumerate(sorted([key for key in result.keys() if not isinstance(result[key],Table)],key=result.get)):
+    for i,k in enumerate(sorted([key for key in result.keys()],key=result.get)):
         buff[k]=i
     result.update(buff)
     return result
-    
+
 def reverse_table(table):
     '''
     This function returns the sequence-index table for a reversed lookup.
     '''
     result=Table()
     for k,v in table.iteritems():
-        if isinstance(v,Table):
-            result[k]=reverse_table(v)
-        else:
-            result[v]=k
+        result[v]=k
     return result
