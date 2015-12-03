c985d0e5c70b08ef396dd591180c493b60b268ee
waltergu <waltergu1989@gmail.com>
2015-12-04 03:01:09 +0800

    BasicGeometyPy doc modified.

--- Core/BasicClass/BasicGeometryPy.py
+++ Core/BasicClass/BasicGeometryPy.py
@@ -138,6 +138,7 @@ class Point:
 
     def table(self,nambu=False,priority=None):
         '''
+        This method returns a Table instance that contains all the allowed indices which can be defined on this point.
         '''
         return self.struct.table(site=self.site,scope=self.scope,nambu=nambu,priority=priority)
 
